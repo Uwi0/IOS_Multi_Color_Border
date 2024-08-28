3ec2fba124903621278f35665dc9a4061f7a5a3c
uwi0 <kakapoweb@gmail.com>
2024-08-28 19:28:46 +0700

    Finishing Multi Color Border

--- Multi Color Border/ContentView.swift
+++ Multi Color Border/ContentView.swift
@@ -1,19 +1,52 @@
-//
-//  ContentView.swift
-//  Multi Color Border
-//
-//  Created by dwi prasetyo on 28/08/24.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    
+    let n = 6
+    let colors: [Color] = [
+        .green,.purple,.cyan,.pink,.teal,.blue,.yellow,.indigo,.orange,.brown
+    ]
+    
+    var delta: Double {
+        1 / Double(n)
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Players")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            Spacer()
+            ZStack {
+                Capsule()
+                    .fill(.purple.opacity(0.3))
+                ForEach(0 ..< n, id: \.self) { i in
+                    let start: Double = delta * Double(i)
+                    let end: Double = start + delta
+                    let color = colors[i%colors.count]
+                    Capsule()
+                        .trim(from: start, to: end)
+                        .stroke(color, lineWidth: 10)
+                        .blur(radius: 25)
+                }
+                ForEach(0 ..< n, id: \.self) { i in
+                    
+                    let start: Double = delta * Double(i)
+                    let end: Double = start + 0.01
+                    let color = colors[i%colors.count]
+                    
+                    let strokeStyle = {(lineWidth: CGFloat) -> StrokeStyle in StrokeStyle(lineWidth: lineWidth, lineCap: .round)
+                    }
+                    
+                    
+                    Capsule()
+                        .trim(from: start, to: end)
+                        .stroke(style: strokeStyle(30))
+                        .stroke(color,style: strokeStyle(5))
+                }
+            }
+            .frame(width: 200, height: 300)
+            Spacer()
         }
         .padding()
     }
