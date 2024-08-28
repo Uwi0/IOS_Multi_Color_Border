6a88772177724a0b3c665922143ce7f9eca031ef
uwi0 <kakapoweb@gmail.com>
2024-08-28 19:08:11 +0700

    Initial Commit

---
+++ Multi Color Border/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Multi Color Border
+//
+//  Created by dwi prasetyo on 28/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Multi Color Border/Multi_Color_BorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Multi_Color_BorderApp.swift
+//  Multi Color Border
+//
+//  Created by dwi prasetyo on 28/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Multi_Color_BorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
